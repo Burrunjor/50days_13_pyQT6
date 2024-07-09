91f99435c541e2a03edf82b2b35639cfca9293cf
drewgarden <amarto@protonmail.com>
2024-07-09 14:19:10 +1000

    Final commit, finished

--- main.py
+++ main.py
@@ -3,16 +3,20 @@
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, \
     QLineEdit, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem, QDialog, \
-    QVBoxLayout, QComboBox, QToolBar, QStatusBar
+    QVBoxLayout, QComboBox, QToolBar, QStatusBar, QMessageBox
 import sys
 from PyQt6.QtGui import QAction, QIcon
 import sqlite3
+
+# The about thing isn't working, just killing the app, dont know why
+# The select is a bit F'd because it doesn't deselected once you've done selecting, or something, couldn't be bothered
+# If you want to use this, there's enought to go from here
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Student Management System")
-        self.setMinimumSize(400, 800)
+        self.setMinimumSize(500, 800)
 
         file_menu_item = self.menuBar().addMenu("&File")
         help_menu_item = self.menuBar().addMenu("&Help")
@@ -26,11 +30,17 @@ class MainWindow(QMainWindow):
         # the about action
         about_action = QAction("About", self)
         help_menu_item.addAction(about_action)
+        about_action.triggered.connect(self.about)
 
         # the search action
         search_action = QAction(QIcon("icons/search.png"), "Search", self)
-        edit_menu_item.addAction(search_action)
         search_action.triggered.connect(self.search)
+
+        #The edit action
+        edit_menu_item.addAction(search_action)
+        edit_menu_item.addAction("Edit record")
+        edit_menu_item.triggered.connect(self.edit)
+
 
         # creating the table and setting this as the central widget
         self.table = QTableWidget()
@@ -55,7 +65,7 @@ class MainWindow(QMainWindow):
 
 
     def load_data(self):
-        connection = sqlite3.connect("database.db")
+        connection = DatabaseConnection().connect()
         result = connection.execute("SELECT * FROM students")
         self.table.setRowCount(0)
         for row_number, row_data in enumerate(result):
@@ -98,10 +108,68 @@ class MainWindow(QMainWindow):
         dialog = DeleteDialog()
         dialog.exec()
 
+    def about(self):
+        dialog = AboutDialog()
+        dialog.exec()
+
+class DatabaseConnection:
+    def __init__(self, database_file="database.db"):
+        self.database_file = database_file
+
+    def connect(self):
+        connection = sqlite3.connect(self.database_file)
+        return connection
+
+
+class AboutDialog(QMessageBox):
+    # inherrtiing from the class, or just instatiating an instance, the former is more tidy I guess
+    def __init__(self):
+        super().__init__()
+        self.windowTitle("About")
+        content = """
+        This app was created in "The Python Mega Course" feel free to modify and reuse
+        """
+        self.setText(content)
 
 
 class DeleteDialog(QDialog):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Delete Student Data")
+
+        layout = QGridLayout()
+        confirmation = QLabel("Are you sure you want to delete?")
+        yes = QPushButton("Yes")
+        no = QPushButton("No")
+
+        layout.addWidget(confirmation, 0, 0, 1, 2)
+        layout.addWidget(yes, 1, 0)
+        layout.addWidget(no, 1, 1)
+        self.setLayout(layout)
+
+        yes.clicked.connect(self.delete_student)
+
+    def delete_student(self):
+        # Get row index and student ID from row selected
+        index = main_window.table.currentRow()
+        student_id = main_window.table.item(index, 0).text()
+        connection = DatabaseConnection().connect()
+        cursor = connection.cursor()
+        cursor.execute("DELETE from students WHERE id = ?", (student_id, ))
+        connection.commit()
+        cursor.close()
+        connection.close()
+        main_window.load_data()
+
+        # adding a confirmation
+        self.close()        # closes the current dialogue window
+        confirmation_widget = QMessageBox()
+        confirmation_widget.setWindowTitle("Success")
+        confirmation_widget.setText("Record deleted")
+        confirmation_widget.exec()
+
+
+
 
 
 class EditDialog(QDialog):
@@ -144,8 +212,10 @@ class EditDialog(QDialog):
         self.setLayout(layout)
 
 
+
+
     def update_student(self):
-        connection = sqlite3.connect("database.db")
+        connection = DatabaseConnection().connect()
         cursor = connection.cursor()
         cursor.execute("UPDATE students SET name = ?, course = ?, mobile = ? WHERE id = ?",
                        (self.student_name.text(),
@@ -157,6 +227,13 @@ class EditDialog(QDialog):
         connection.close()
         # refresh the table
         main_window.load_data()
+
+        # Adding a confirmation
+        self.close()  # closes the current dialogue window
+        confirmation_widget = QMessageBox()
+        confirmation_widget.setWindowTitle("Success")
+        confirmation_widget.setText("Record edited")
+        confirmation_widget.exec()
 
 
 
@@ -183,7 +260,7 @@ class SearchDialog(QDialog):
         # Note the stuff edited out seems to be just about showing how to selecct things using a search
         # actually I dont know why he did that, because he doesn't seem to use it
         name = self.student_name.text()
-        # connection = sqlite3.connect("database.db")
+        # connection = DatabaseConnection().connect()
         # cursor = connection.cursor()
         # result = cursor.execute("SELECT * FROM students WHERE name =?", (name,))
         # rows = list(result)
@@ -232,7 +309,7 @@ class InsertDialog(QDialog):
         name = self.student_name.text()
         course = self.course_name.itemText(self.course_name.currentIndex())
         mobile = self.mobile.text()
-        connection = sqlite3.connect("database.db")
+        connection = DatabaseConnection().connect()
         cursor = connection.cursor()
         cursor.execute("INSERT INTO students (name, course, mobile) VALUES (?, ?, ?)",
                        (name, course, mobile))
@@ -240,6 +317,13 @@ class InsertDialog(QDialog):
         cursor.close()
         connection.close()
         main_window.load_data()
+
+        # adding a confirmation
+        self.close()  # closes the current dialogue window
+        confirmation_widget = QMessageBox()
+        confirmation_widget.setWindowTitle("Success")
+        confirmation_widget.setText(f"New student {name} added")
+        confirmation_widget.exec()
 
 
 app = QApplication(sys.argv)
